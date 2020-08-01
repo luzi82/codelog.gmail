a25bf5887b69819237b1dc2de19a0760c7ab6a45
luzi82 <luzi82@gmail.com>
2020-08-01 15:49:44 +0800

    fix refresh exception

--- search_email.py
+++ search_email.py
@@ -26,14 +26,19 @@ def main():
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
+
+    # try refresh creds
+    try:
+        if creds and (not creds.valid) and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+    except:
+        creds = None
+
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
